1e0a0e68a55098911e77dc142c611630b68ebcc6
Alfredo Paz <apazv@unsa.edu.pe>
2023-04-27 06:47:32 -0500

    main parameters were fixed

--- 01/Hello.java
+++ 01/Hello.java
@@ -2,7 +2,7 @@
  *This is my first program in Java programming language
  */
 class Hello {
-  public static void main(){
+  public static void main(String[] args){
     System.out.println("Hello World!");
   }
 }
